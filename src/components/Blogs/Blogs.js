d65004ffd9d36b6cf05cbc5d5a71c1a5e1e3dc17
dev-sibli <siblisadik13@gmail.com>
2022-04-04 23:27:58 +0600

    second question answered

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,14 +3,27 @@ import React from 'react';
 const Blogs = () => {
 	return (
 		<div className="container mx-auto mt-5">
-			<h1 className="text-4xl">What is Context API? How It's works?</h1>
-			<p>
-				Normally in react application data is passed top-down (parent to child)
-				via props, but that can be problematic for certain types of props. via
-				props data passed to parent to children. With Context we can share one
-				state across the entire app easily. We don't need to pass props down
-				manually at every level.
-			</p>
+			<div className="my-5">
+				<h1 className="text-4xl">What is Context API? How It's works?</h1>
+				<p>
+					Normally in react application data is passed top-down (parent to
+					child) via props, but that can be problematic for certain types of
+					props. via props data passed to parent to children. With Context we
+					can share one state across the entire app easily. We don't need to
+					pass props down manually at every level.
+				</p>
+			</div>
+			<div>
+				<h1 className="text-4xl">What is Semantic Tag? </h1>
+				<p>
+					Semantic HTML elements are those that clearly describe their meaning
+					in a human- and machine-readable way. Elements such as 'header' ,
+					'footer' and 'article' are all considered semantic because they
+					accurately describe the purpose of the element and the type of content
+					that is inside them. it is much easier to read. semantic elements are
+					easier to understand for human, Search engines etc
+				</p>
+			</div>
 		</div>
 	);
 };
